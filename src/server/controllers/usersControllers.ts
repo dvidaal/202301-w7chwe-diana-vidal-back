a6976e918df83618005a83edbe8d48a9a03303b9
Diana Vidal <dvidaal@gmail.com>
2023-02-25 18:57:15 +0100

    Add create user function

--- src/server/controllers/usersControllers.ts
+++ src/server/controllers/usersControllers.ts
@@ -1,8 +1,14 @@
+import bcryptjs from "bcryptjs";
 import { type Request, type Response, type NextFunction } from "express";
 import { CustomError } from "../../CustomError/CustomError.js";
 import User from "../../database/models/User.js";
+import { type UserStructure } from "../types.js";
 
-const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+export const getUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const users = await User.find();
 
@@ -17,4 +23,32 @@ const getUsers = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default getUsers;
+export const createUser = async (
+  req: Request<Record<string, unknown>, Record<string, unknown>, UserStructure>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const hashedPassword = await bcryptjs.hash(password, 8);
+    const avatar = req.file?.originalname;
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      avatar,
+    });
+
+    res.status(201).json({ username });
+  } catch (error) {
+    const customError = new CustomError(
+      (error as Error).message,
+      500,
+      "Impossible to create a user"
+    );
+
+    next(customError);
+  }
+};
